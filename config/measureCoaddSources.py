7fe4e4de84d916c2c75ab8f82c8b4355e1fa52e3
Paul Price <price@astro.princeton.edu>
2018-03-30 12:34:17 -0400

    config: use custom Pan-STARRS refcat loader
    This allows the proper motion correction to be done using the non-standard
    parameterisation in the ps1_pv3_3pi_20170110 catalog.

--- config/measureCoaddSources.py
+++ config/measureCoaddSources.py
@@ -1,5 +1,5 @@
-from lsst.meas.algorithms import LoadIndexedReferenceObjectsTask
-config.match.refObjLoader.retarget(LoadIndexedReferenceObjectsTask)
+from lsst.meas.algorithms import LoadPanstarrsObjectsTask
+config.match.refObjLoader.retarget(LoadPanstarrsObjectsTask)
 config.match.refObjLoader.ref_dataset_name = "ps1_pv3_3pi_20170110"
 config.match.refObjLoader.filterMap['u'] = 'g'
 config.match.refObjLoader.filterMap['Y'] = 'y'
--- config/processCcd.py
+++ config/processCcd.py
@@ -1,7 +1,7 @@
 import os.path
 
 from lsst.utils import getPackageDir
-from lsst.meas.algorithms import LoadIndexedReferenceObjectsTask
+from lsst.meas.algorithms import LoadPanstarrsObjectsTask
 from lsst.obs.decam.isr import DecamIsrTask
 config.isr.retarget(DecamIsrTask)
 
@@ -14,7 +14,7 @@ for refObjLoader in (config.calibrate.astromRefObjLoader,
                      config.calibrate.photoRefObjLoader,
                      config.charImage.refObjLoader,
                      ):
-    refObjLoader.retarget(LoadIndexedReferenceObjectsTask)
+    refObjLoader.retarget(LoadPanstarrsObjectsTask)
     refObjLoader.ref_dataset_name = "ps1_pv3_3pi_20170110"
     # Note the u-band results may not be useful without a color term
     refObjLoader.filterMap['u'] = 'g'
